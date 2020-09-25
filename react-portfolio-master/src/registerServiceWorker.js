8ae91fbf14d2f74bcca0010e12b13d10453e080a
MarigonaN <marigonanuhija@gmail.com>
2020-09-25 14:57:40 +0200

    commit

--- react-portfolio-master/src/registerServiceWorker.js
+++ react-portfolio-master/src/registerServiceWorker.js
@@ -1,5 +1,4 @@
 
-
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
@@ -15,9 +14,7 @@ export default function register() {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
-      // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is served on. This might happen if a CDN is used to
-      // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
+   
       return;
     }
 

62ad0539b258704e7723cd937388a99905a7623c
MarigonaN <marigonanuhija@gmail.com>
2020-09-25 15:00:14 +0200

    commit

--- react-portfolio-master/src/Components/Footer.js
+++ react-portfolio-master/src/Components/Footer.js
@@ -23,8 +23,8 @@ class Footer extends Component {
            </ul>
 
            <ul className="copyright">
-              <li>&copy; All credit goes to Tim Baker - Copyright 2017 Tim Baker</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>&copy; All credit goes to Marigona Nuhija</li>
+             
            </ul>
 
         </div>

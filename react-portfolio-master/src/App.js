1c71ff36cdd235375633d8a5454ceb35806a3e6a
MarigonaN <marigonanuhija@gmail.com>
2020-10-14 14:32:19 +0200

    commit

--- react-portfolio-master/src/App.js
+++ react-portfolio-master/src/App.js
@@ -6,8 +6,6 @@ import Header from './Components/Header';
 import Footer from './Components/Footer';
 import About from './Components/About';
 import Resume from './Components/Resume';
-import Contact from './Components/Contact';
-
 import Portfolio from './Components/Portfolio';
 
 class App extends Component {
@@ -47,8 +45,6 @@ class App extends Component {
         <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
         <Portfolio data={this.state.resumeData.portfolio}/>
-      
-        <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
     );
